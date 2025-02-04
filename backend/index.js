6f4710e532630550bb68bad6496686bdfb52dadf
rakeshshah18 <21amtics464@gmail.com>
2025-02-04 22:11:15 +0530

    try to host projrct to vercel app

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors(
+    {
+        origin: ['https://cm-placement-services.vercel.app/'],
+        methods: ['POST', 'GET'],
+        credentials: true
+    }
+));
 app.use(express.json());
 
 // Database Connection
--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -4,7 +4,7 @@ dotenv.config();
 
 const connection_DB = async () => {
     try {
-        const MONGO_URI = 'mongodb://127.0.0.1:27017/cmServices';
+        const MONGO_URI = 'mongodb+srv://21amtics464:root1@jobs.sepmv.mongodb.net/?retryWrites=true&w=majority&appName=jobs';
         await mongoose.connect(MONGO_URI, {});
         console.log("Connected to MongoDB.");
     } catch (error) {
